73a5ee1c43bd86bc50f26157775a4fafa2f7fab4
Zsolt <szabzsolt1021@gmail.com>
2022-05-08 20:02:00 +0200

    alap

---
+++ KliensSzerver_Orvos/Orvos_Asszisztens_Szerver/Repositories/PatientRepository.cs
@@ -0,0 +1,30 @@
+﻿using Common_Library.Models;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+
+namespace Orvos_Asszisztens_Szerver.Repositories
+{
+    public static class PatientRepository
+    {
+        private const string FileName = "Patients.json";
+
+        public static IEnumerable<Patient> GetPatients()
+        {
+            if(File.Exists(FileName))
+            {
+                var rawData = File.ReadAllText(FileName);
+                var patients = JsonSerializer.Deserialize<IEnumerable<Patient>>(rawData);
+                return patients;
+            }
+
+            return new List<Patient>();
+        }
+
+        public static void StorePatients(IEnumerable<Patient> patients)
+        {
+            var rawData = JsonSerializer.Serialize(patients);
+            File.WriteAllText(FileName, rawData);
+        }
+    }
+}
