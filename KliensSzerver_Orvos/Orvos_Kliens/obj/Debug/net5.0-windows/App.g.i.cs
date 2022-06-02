e6b46506644b5d5cd7acfc3b9f3e2c8a432cde00
czegeny.patrik <czegeny.patrik98@gmail.com>
2022-06-02 15:36:52 +0200

    név és taj validation.

--- KliensSzerver_Orvos/Orvos_Kliens/obj/Debug/net5.0-windows/App.g.i.cs
+++ KliensSzerver_Orvos/Orvos_Kliens/obj/Debug/net5.0-windows/App.g.i.cs
@@ -45,7 +45,7 @@ namespace Orvos_Kliens {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.2.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.5.0")]
         public void InitializeComponent() {
             
             #line 5 "..\..\..\App.xaml"
@@ -60,7 +60,7 @@ namespace Orvos_Kliens {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.2.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.5.0")]
         public static void Main() {
             Orvos_Kliens.App app = new Orvos_Kliens.App();
             app.InitializeComponent();
